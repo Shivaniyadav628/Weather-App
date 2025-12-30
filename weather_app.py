91a3ff788b9ad636cb0b3e9027e2604ac2777a13
Shivaniyadav628 <shivanigundemoni68@gmail.com>
2025-12-30 18:27:02 +0530

    Enhanced weather app with analytics and code quality

--- weather_app.py
+++ weather_app.py
@@ -10,7 +10,7 @@ CSV_FILE = "weather_history.csv"
 
 API_KEY = os.getenv("OPENWEATHER_API_KEY")
 print("DEBUG API Key:", API_KEY)
-print("DEBUG API Key:", API_KEY)
+
 # ---------------- CSV INITIALIZATION ----------------
 
 if not os.path.exists(CSV_FILE):
